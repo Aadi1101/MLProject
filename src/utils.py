b5ba54440f90832ee4ed2c42949f91d216fa3461
Aadi1101 <aadityakomerwar@gmail.com>
2023-04-20 20:23:35 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,4 @@
-import os,sys,numpy as np, pandas as pd,dill
+import os,sys,numpy as np, pandas as pd,pickle
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
@@ -9,7 +9,7 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
@@ -44,3 +44,11 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e,sys)
